c3f3bc103b8ae875f9c17c0f45eefe3bbc0147b6
ymcagodme <ymcagodme@gmail.com>
2012-08-28 23:23:05 -0700

    half way to solve beads

--- beads/beads.c
+++ beads/beads.c
@@ -8,25 +8,40 @@ PROG: beads
 #include<stdlib.h>
 #define MAXBEAD 800
 
-main () {
+int main () {
     FILE *fin = fopen("beads.in", "r");
     FILE *fout = fopen("beads.out", "w");
-    int i, beadCount;
+    int i, beadCount, maxCount = 0;
     char beads[MAXBEAD];
 
     fscanf(fin, "%d", &beadCount);
     fscanf(fin, "%s", beads);
-    printf("Count: %d\n", beadCount);
     printf("%s\n", beads);
 
-    // Extend it with same beads twice
     for (i = 0; i < beadCount; i++) {
-        beads[beadCount + i] = beads[i];
-    }
+        char head = beads[i];
+        int count = 0;
+        int j;
+        int isOriginalHead = 1;
+        for (j = 0; j < beadCount; j++) {
+            char nextChar = beads[ (i + j) % beadCount ];
+            if (head == 'w') {
+                head = nextChar;
+                count++;
+            }
+            else if( head ==  nextChar || nextChar == 'w' || isOriginalHead ){
+                if(head != nextChar){
+                    head = nextChar;
+                    isOriginalHead = 0;
+                }
+                count++;
+            }
+        }
+        printf("@%d:\tcount:%d\thead:%c\n", i, count, head);
 
-    for (i = 0; i < beadCount; i++) {
-        int c = 1;
+        if( count > maxCount)
+            maxCount = count;
     }
-
+    printf("%d\n", maxCount);
     exit (0);
 }
