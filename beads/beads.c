87e41d8b99d500ed8387048658453aaee08fd96f
ymcagodme <ymcagodme@gmail.com>
2012-08-29 00:26:22 -0700

    Finish Broken Necklace(1.1)

--- beads/beads.c
+++ beads/beads.c
@@ -16,32 +16,45 @@ int main () {
 
     fscanf(fin, "%d", &beadCount);
     fscanf(fin, "%s", beads);
-    printf("%s\n", beads);
 
+    // Scan each char
     for (i = 0; i < beadCount; i++) {
         char head = beads[i];
         int count = 0;
         int j;
+
+        // This is an identifier for turning into another color. r -> b or b -> r
         int isOriginalHead = 1;
+
+        // From a specific postion(beads[i]) to go down
         for (j = 0; j < beadCount; j++) {
+
+            // Circling count. Because it is counted from 0, the first nextChar will be itself.
+            // As a result, the logic( count++ ) will be correct. The (int)count is "like" starting from 1.
             char nextChar = beads[ (i + j) % beadCount ];
             if (head == 'w') {
                 head = nextChar;
                 count++;
             }
+            // Make sure it only turns into another color once. (except white)
             else if( head ==  nextChar || nextChar == 'w' || isOriginalHead ){
-                if(head != nextChar){
+                // If it came from the reason that isOriginalHead is true, then marking used.
+                if( nextChar != 'w' && head != nextChar){
                     head = nextChar;
                     isOriginalHead = 0;
                 }
                 count++;
             }
+            // Third condition is that meeting to the third times of changing color.
+            // Then, it should break out of the loop.
+            else{
+                break;
+            }
         }
-        printf("@%d:\tcount:%d\thead:%c\n", i, count, head);
 
         if( count > maxCount)
             maxCount = count;
     }
-    printf("%d\n", maxCount);
+    fprintf(fout, "%d\n", maxCount);
     exit (0);
 }
